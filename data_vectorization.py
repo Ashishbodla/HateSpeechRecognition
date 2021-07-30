f91fd0319e9d142dfb45f35c746253516fe0d8f9
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-07-30 12:55:41 +0530

    Count vectorizer
    As a part of data vectorization, count vector is used in this file

---
+++ data_vectorization.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+train = pd.read_csv('Training_data.csv')
+
+#Vectorizer class initiation
+word_vec0 = CountVectorizer(ngram_range=(1,2), analyzer='word')
+
+#Fitting and transforming the train data
+mat_tr = word_vec.fit_transform(train['clean_txt'])
+
+#processing the vectorizer output
+#finding the frequency of words in the train data
+freq = sum(mat_tr).toarray()[0]
+df=pd.DataFrame(freq, index=word_vec.get_feature_names(), columns=['frequency'])
+df1=pd.DataFrame(mat_tr.todense(), columns=[word_vec.get_feature_names()])
+ind = df[(df['frequency']>5) & (df['frequency']<50)].index #limiting the frequency
+df2 = df1[np.array(ind)] #taking only the required terms based on frequency filtering
+df2.reset_index(drop=True, inplace=True)
+train_data_final = pd.concat([df2,train[['Hate','Offensive','Neither']]],axis=1)
+train_data_final.to_csv('Train_data_vectored.csv')
