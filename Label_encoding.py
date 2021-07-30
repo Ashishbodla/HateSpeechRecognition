2f127f96496696b434689df2ee543fbfe58c8b50
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-07-30 10:48:35 +0100

    Update Label_encoding.py

--- Label_encoding.py
+++ Label_encoding.py
@@ -7,7 +7,7 @@ test = pd.read_csv('Test_data.csv')
 
 #Creating dummies i.e. one hot encoding and concatenating to the original file
 train = pd.concat([train,pd.get_dummies(train['class'])], axis=1)
-test = pd.concat([test,pd.get_dummies(test['class']], axis=1)
+test = pd.concat([test,pd.get_dummies(test['class'])], axis=1)
 
 #Renaming the dummy columns
 train.rename(columns={0:'Hate',1:'Offensive',2:'Neither'}, inplace=True)
