9bb353c247e72a04cef057a592130487c7c6430b
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-04 17:14:24 +0530

    Knn classifier
    Create knn classifiers for different K

---
+++ knn_classifier.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix 
+import numpy as np
+
+
+train = pd.read_csv(r"CountVectorised_train.csv")
+test = pd.read_csv(r"CountVectorised_test.csv")
+
+train.dropna(inplace=True)
+test.dropna(inplace=True)
+
+X_train = train.drop(columns = ['class','Hate','Offensive','Neither'])
+y_train = train['class']
+X_test = test.drop(columns = ['class','Hate','Offensive','Neither'])
+y_test = test['class',]
+
+# Model building for each number of neighbors
+
+for i in range(3,20):
+    neigh = KNeighborsClassifier(n_neighbors=i)
+    neigh.fit(X_train, y_train)
+
+    y_pred = neigh.predict(X_test)
+    C = confusion_matrix(y_test,y_pred)
+    print(i,np.trace(np.matrix(C)))
