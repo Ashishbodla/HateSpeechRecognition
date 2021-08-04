5255c5802c6f7cdcc5510f6ccf04055e5cba94a0
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-04 17:18:15 +0530

    Create svm_classifier.py
    SVM classifier

---
+++ svm_classifier.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+train = pd.read_csv(r"CountVectorised_train.csv")
+test = pd.read_csv(r"CountVectorised_test.csv")
+
+train.dropna(inplace=True)
+test.dropna(inplace=True)
+
+X_train = train.drop(columns = ['class','Hate','Offensive','Neither'])
+y_trian = train['class']
+
+X_test = test.drop(columns = ['class','Hate','Offensive','Neither'])
+y_test = test['class']
+
+from sklearn import svm
+
+clf = svm.SVC()
+clf.fit(X_train, y_trian)
+y_Pred_svm = clf.predict(X_test)
+C_svm = confusion_matrix(y_test,y_Pred_svm)
+print(np.trace(np.matrix(C_svm))
