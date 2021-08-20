37a972d18e2e4ac22f2f4d519f248579139a9f63
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-20 13:17:48 +0530

    Create test_individual_tweets.py
    test individual cases with the input tweet

---
+++ test_individual_tweets.py
@@ -0,0 +1,24 @@
+#To test tf-idf Vectorizer 
+# To test models on count vectorizer
+import pickle
+import numpy as np
+import pandas as pd 
+
+t_vec = pickle.load(open('TFIDF_vectorizer.sav','rb'))
+with open('Count_vector_indices.npy','rb') as f:
+    ind = np.load(f,allow_pickle=True)
+
+pred_map = {0:'Hate',1:'Offensive',2:'Neither'}
+
+c_vec = pickle.load(open('Count_Vectorizer.sav','rb'))
+with open('Count_vector_indices.npy','rb') as f:
+    ind = np.load(f,allow_pickle=True)
+
+input_text = input('Test tweet: ')
+x1 = c_vec.transform(np.array([input_text]))
+df1 = pd.DataFrame(x1.todense(),columns=word_vec.get_feature_names())
+df = df1[ind]
+#Import a model to be tested
+x2 = t_vec.transform(df)
+model = pickle.load(open('./tfidf_vectors_models/MNB_counts.sav','rb'))
+print('The tweet: ', input_text, '- is ', pred_map[model.predict(x2)[0]])
