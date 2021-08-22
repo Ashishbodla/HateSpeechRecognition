5942c3d82d1d32d2d4ea4668f346b1385057413a
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-22 19:36:05 +0100

    Update test_train_split.py

--- test_train_split.py
+++ test_train_split.py
@@ -7,5 +7,6 @@ df = pd.read_csv('Cleaned_text.csv',usecols=['class','clean_txt']) #import data
 X_train, X_test, y_train, y_test = train_test_split(df['clean_txt'], df['class'], test_size=0.20, random_state=42) 
 
 #concat and save the files
-pd.concat([X_train,y_train],axis=1).to_csv('Training_data.csv')
-pd.concat([X_test,y_test],axis=1).to_csv('Test_data.csv')
+pd.concat([X_train,y_train],axis=1).to_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Training_data.csv',index=False)
+pd.concat([X_test,y_test],axis=1).to_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Test_data.csv',index=False)
+
