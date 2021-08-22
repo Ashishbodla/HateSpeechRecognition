c18df209965d38461cd96df77656c00c67ccf39e
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-22 19:36:34 +0100

    Update tf_idf_models.py

--- tf_idf_models.py
+++ tf_idf_models.py
@@ -6,14 +6,17 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 import pickle
 
-train = pd.read_csv(r'/Users/nageswar/Desktop/Hate Speech/HateSpeech-3/tfidf_vectored_train.csv')
+train = pd.read_csv(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/tfidf_vectored_train.csv')
 
-test = pd.read_csv(r'/Users/nageswar/Desktop/Hate Speech/HateSpeech-3/tfidf_vectored_test.csv')
+test = pd.read_csv(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/tfidf_vectored_test.csv')
 train.dropna(inplace=True)
 test.dropna(inplace=True)
 
 
-
+X_train = train.drop(columns = ['class','Hate','Offensive','Neither'])
+y_train = train['class']
+X_test = test.drop(columns = ['class','Hate','Offensive','Neither'])
+y_test = test['class']
 tfidf = TfidfTransformer()
 
 x_train_tfidf = tfidf.fit_transform(X_train)
@@ -39,7 +42,7 @@ for i in range(3,20):
     y_pred = neigh.predict(X_test)
     C = confusion_matrix(y_test,y_pred)
     print(i,np.trace(np.matrix(C)))
-    pickle.dump(neigh,open(r'.\tfidf_vectors_models\Neigh_{}.sav'.format(i),'wb'))
+    pickle.dump(neigh,open(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/models/tfidfmodels/Neigh_{}.sav'.format(i),'wb'))
 
 from sklearn import svm
 
@@ -48,7 +51,7 @@ clf.fit(X_train, y_train)
 y_Pred_svm = clf.predict(X_test)
 C_svm = confusion_matrix(y_test,y_Pred_svm)
 print(np.trace(np.matrix(C_svm)))
-pickle.dump(clf,open(r'.\tfidf_vectors_models\SVM_SVC.sav','wb'))
+pickle.dump(clf,open(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/models/tfidfmodels/SVM_SVC.sav','wb'))
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
@@ -58,7 +61,7 @@ MNB= MultinomialNB().fit(X_train, train['class'])
 MNB_preds = MNB.predict(X_test)
 print(confusion_matrix(test['class'],MNB_preds))
 print (classification_report(test['class'], MNB_preds))
-pickle.dump(MNB, open(r'.\tfidf_vectors_models\MNB_counts.sav','wb'))
+pickle.dump(MNB, open(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/models/tfidfmodels/MNB_counts.sav','wb'))
 
 from sklearn.ensemble import RandomForestClassifier
 
@@ -68,4 +71,4 @@ for N in range(100,1000,100):
     rfc_preds = rfc.predict(X_test)
     C_rfc = confusion_matrix(y_test,rfc_preds)
     print(N,np.trace(np.matrix(C_rfc)))
-    pickle.dump(rfc, open(r'.\tfidf_vectors_models\RFC_{}.sav'.format(N),'wb'))
+    pickle.dump(rfc, open(r'/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/models/tfidfmodels/RFC_{}.sav'.format(N),'wb'))
