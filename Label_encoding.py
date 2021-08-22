df12d0e17e2d4d053695c7d9ee189bacca8bdfb3
nageswar123 <77968908+nageswar123@users.noreply.github.com>
2021-08-22 19:30:25 +0100

    Update Label_encoding.py

--- Label_encoding.py
+++ Label_encoding.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 #Importing the data files from train test split output
 
-train = pd.read_csv('Training_data.csv')
-test = pd.read_csv('Test_data.csv')
+train = pd.read_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Training_data.csv')
+test = pd.read_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Test_data.csv')
 
 #Creating dummies i.e. one hot encoding and concatenating to the original file
 train = pd.concat([train,pd.get_dummies(train['class'])], axis=1)
@@ -18,5 +18,5 @@ test.rename(columns={0:'Hate',1:'Offensive',2:'Neither'}, inplace=True)
 #test.drop(columns=['Unnamed: 0'], inplace = True)
 
 #Saving the files
-train.to_csv('Training_data.csv', index=False)
-test.to_csv('Test_data.csv', index=False)
+train.to_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Training_data.csv', index=False)
+test.to_csv('/Users/umeshkethepalli/Desktop/Hate Speech/HateSpeech-5/data_files/Test_data.csv', index=False)
